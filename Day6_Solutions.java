f4495feb11cf51d39dd65303c84e8e51bcaf7785
Dhanusree <dhanudhanu1903@gmail.com>
2025-06-14 16:21:19 +0530

    Basic Array_problems

--- Day6_Solutions.java
+++ Day6_Solutions.java
@@ -1,2 +1,25 @@
-**Problems Solved (7 total):**
-1. [Build Array from Permutation](https://leetcode.com/problems/build-array-from-permutation/)
+// Day 6: LeetCode Solutions
+
+// 1920. Build Array from Permutation
+// Problem Link: https://leetcode.com/problems/build-array-from-permutation/
+// My Solution (Discuss): https://leetcode.com/submissions/detail/1663668977/
+
+//  744. Find Smallest Letter Greater Than Target
+// Problem Link: https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+// My Solution (Discuss): https://leetcode.com/submissions/detail/1663498693/
+
+//  1929. Concatenation of Array
+// Problem Link: https://leetcode.com/problems/concatenation-of-array/
+// My Solution (Discuss):https://leetcode.com/submissions/detail/1663680213/
+
+//  1480. Running Sum of 1d Array
+// Problem Link: https://leetcode.com/problems/running-sum-of-1d-array/
+// My Solution (Discuss): https:https://leetcode.com/submissions/detail/1663688341/
+
+//  1672. Richest Customer Wealth
+// Problem Link: https://leetcode.com/problems/richest-customer-wealth/
+// My Solution (Discuss): https://leetcode.com/submissions/detail/1663701866/
+
+//  1470. Shuffle the Array
+// Problem Link: https://leetcode.com/problems/shuffle-the-array/
+// My Solution (Discuss): https://leetcode.com/submissions/detail/1663723429/
